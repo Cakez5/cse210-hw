f23402506793dfdfec9714aa8556dec8002504bb
Cakez5 <zmartinsen@byupathway.edu>
2025-05-09 16:47:51 -0600

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello Welcome to the Number Guess Game!");
+        Random randomGenerator = new Random();
+        int RandomNumber = randomGenerator.Next(1, 101);
+     
+        int guess = -1;
+
+        while (guess != RandomNumber)
+        {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (RandomNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (RandomNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgain = Console.ReadLine().ToLower();
+
+            if (playAgain == "yes")
+        {
+            RandomNumber = randomGenerator.Next(1, 101); // Generate a new number
+            guess = -1; // Reset guess
+        }
+            else
+        {
+            break; // Exit the loop
+        }
+        }
+        }
     }
 }
