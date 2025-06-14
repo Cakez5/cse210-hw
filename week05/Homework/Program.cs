28da5f2df07bd304f5c6245a3ed23741a47c4ef7
Cakez5 <zmartinsen@byupathway.edu>
2025-06-14 14:55:15 -0600

    finished code.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test base class
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
